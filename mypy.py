79b70fa7b08589db972fd073764695452d9b0eb7
kaustuvbanerjee <kbanerjee@utilitywarehouse.co.uk>
2016-09-15 16:40:44 +0100

    Add files via upload

---
+++ mypy.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+
+
+for i in range (0,1000):
+	var = raw_input('enter the extension:  ')
+	var2 = 'dir /b/s *.'+ (var)
+	var3 = 'del /s *.'+ (var)
+	print (var3)
+	print ('following file will be deleted.')
+	i = os.system(var2)
+	os.system(var3)
+	break
+
+
